84cec9fd81345474a7afd03b8b6f18054323875e
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-03-17 23:45:57 +0300

    Add project files.

---
+++ Music Player/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Music_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Syncfusion.Windows.Forms.Tools.SliderCollection sliderCollection1 = new Syncfusion.Windows.Forms.Tools.SliderCollection();
+            this.Player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toggleButton1 = new Syncfusion.Windows.Forms.Tools.ToggleButton();
+            this.sfButton1 = new Syncfusion.WinForms.Controls.SfButton();
+            this.sfButton2 = new Syncfusion.WinForms.Controls.SfButton();
+            this.sfButton3 = new Syncfusion.WinForms.Controls.SfButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toggleButton1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.Enabled = true;
+            this.Player.Location = new System.Drawing.Point(-1, -2);
+            this.Player.Name = "Player";
+            this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
+            this.Player.Size = new System.Drawing.Size(771, 244);
+            this.Player.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(-1, 210);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 32);
+            this.textBox1.TabIndex = 1;
+            // 
+            // toggleButton1
+            // 
+            this.toggleButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toggleButton1.ForeColor = System.Drawing.Color.Black;
+            this.toggleButton1.Location = new System.Drawing.Point(89, 248);
+            this.toggleButton1.MinimumSize = new System.Drawing.Size(52, 20);
+            this.toggleButton1.Name = "toggleButton1";
+            this.toggleButton1.Size = new System.Drawing.Size(90, 40);
+            this.toggleButton1.Slider = sliderCollection1;
+            this.toggleButton1.TabIndex = 2;
+            this.toggleButton1.Text = "toggleButton1";
+            this.toggleButton1.Click += new System.EventHandler(this.toggleButton1_Click);
+            // 
+            // sfButton1
+            // 
+            this.sfButton1.AccessibleName = "Button";
+            this.sfButton1.Font = new System.Drawing.Font("Segoe UI Semibold", 14F);
+            this.sfButton1.Location = new System.Drawing.Point(256, 248);
+            this.sfButton1.Name = "sfButton1";
+            this.sfButton1.Size = new System.Drawing.Size(146, 40);
+            this.sfButton1.TabIndex = 3;
+            this.sfButton1.Text = "Browse";
+            this.sfButton1.Click += new System.EventHandler(this.sfButton1_Click);
+            // 
+            // sfButton2
+            // 
+            this.sfButton2.AccessibleName = "Button";
+            this.sfButton2.Font = new System.Drawing.Font("Segoe UI Semibold", 14F);
+            this.sfButton2.Location = new System.Drawing.Point(410, 248);
+            this.sfButton2.Name = "sfButton2";
+            this.sfButton2.Size = new System.Drawing.Size(146, 40);
+            this.sfButton2.TabIndex = 4;
+            this.sfButton2.Text = "Stop";
+            // 
+            // sfButton3
+            // 
+            this.sfButton3.AccessibleName = "Button";
+            this.sfButton3.Font = new System.Drawing.Font("Segoe UI Semibold", 14F);
+            this.sfButton3.Location = new System.Drawing.Point(565, 248);
+            this.sfButton3.Name = "sfButton3";
+            this.sfButton3.Size = new System.Drawing.Size(146, 40);
+            this.sfButton3.TabIndex = 5;
+            this.sfButton3.Text = "Exit";
+            this.sfButton3.Click += new System.EventHandler(this.sfButton3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(772, 298);
+            this.Controls.Add(this.sfButton3);
+            this.Controls.Add(this.sfButton2);
+            this.Controls.Add(this.sfButton1);
+            this.Controls.Add(this.toggleButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Player);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Music Player";
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.toggleButton1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Player;
+        private System.Windows.Forms.TextBox textBox1;
+        private Syncfusion.Windows.Forms.Tools.ToggleButton toggleButton1;
+        private Syncfusion.WinForms.Controls.SfButton sfButton1;
+        private Syncfusion.WinForms.Controls.SfButton sfButton2;
+        private Syncfusion.WinForms.Controls.SfButton sfButton3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Music Player/Form1.cs
@@ -0,0 +1,47 @@
+﻿using Syncfusion.Windows.Forms.Tools;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Music_Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sfButton1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+            Player.URL = textBox1.Text;
+        }
+
+        private void sfButton3_Click(object sender, EventArgs e)
+        {
+           this.Close();
+        }
+
+        private void toggleButton1_Click(object sender, EventArgs e)
+        {
+            if (toggleButton1.ToggleState == ToggleButtonState.Inactive)
+            {
+                Player.Ctlcontrols.pause();             }
+            else
+            {
+                Player.Ctlcontrols.play(); 
+            }
+        }
+        
+    }
+}
